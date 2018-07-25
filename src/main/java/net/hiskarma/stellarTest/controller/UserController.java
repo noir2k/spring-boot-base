d56679a6dbdac70a6ba170e8fcf17b642c1599d4
noir2k <noir2k@gmail.com>
2018-07-25 16:32:37 +0900

    add restcontroller annotation to user model

--- src/main/java/net/hiskarma/stellarTest/controller/UserController.java
+++ src/main/java/net/hiskarma/stellarTest/controller/UserController.java
@@ -2,7 +2,9 @@ package net.hiskarma.stellarTest.controller;
 
 import net.hiskarma.stellarTest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
