afcb613e50f0d0f55d1c9040e9e055ad53d5de9b
zefyr <zefyr@zezz.com>
2018-07-25 20:18:52 +0900

    utility classes added

---
+++ src/main/java/net/hiskarma/stellarTest/util/collection/SingleKeyObject.java
@@ -0,0 +1,9 @@
+package net.hiskarma.stellarTest.util.collection;
+
+/**
+ * Object has Single Key that used for SingleKey Collections
+ * @param <SK>
+ */
+public interface SingleKeyObject<SK> {
+	SK __getSKey__();
+}
