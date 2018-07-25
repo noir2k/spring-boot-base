4f3dd10043846a60a35fef5fbdcdd6c8958e416a
noir2k <noir2k@gmail.com>
2018-07-25 16:18:57 +0900

    make user model entity

--- src/main/java/net/hiskarma/stellarTest/model/User.java
+++ src/main/java/net/hiskarma/stellarTest/model/User.java
@@ -2,7 +2,16 @@ package net.hiskarma.stellarTest.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
 
