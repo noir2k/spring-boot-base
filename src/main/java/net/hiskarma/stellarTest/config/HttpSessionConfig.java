936be3a93ebc6940dbad63af57c94e32f6de557a
noir2k <noir2k@gmail.com>
2018-08-06 18:21:55 +0900

    Fixed MongoSession uses Jackson to serialize session objects to/from JSON

--- src/main/java/net/hiskarma/stellarTest/config/HttpSessionConfig.java
+++ src/main/java/net/hiskarma/stellarTest/config/HttpSessionConfig.java
@@ -1,15 +1,13 @@
 package net.hiskarma.stellarTest.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.session.data.mongo.JdkMongoSessionConverter;
+import org.springframework.session.data.mongo.JacksonMongoSessionConverter;
 import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
-
-import java.time.Duration;
 
 @EnableMongoHttpSession
 public class HttpSessionConfig {
     @Bean
-    public JdkMongoSessionConverter jdkMongoSessionConverter() {
-        return new JdkMongoSessionConverter(Duration.ofMinutes(30));
+    JacksonMongoSessionConverter mongoSessionConverter() {
+        return new JacksonMongoSessionConverter();
     }
 }
