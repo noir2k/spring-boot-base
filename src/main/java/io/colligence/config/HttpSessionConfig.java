c0537fc2500c61acd9662913f1b57d679f462b23
noir2k <noir2k@gmail.com>
2018-08-08 19:38:47 +0900

    Seesion / Cache Data 사용: mongodb => redis 교체

--- src/main/java/io/colligence/config/HttpSessionConfig.java
+++ src/main/java/io/colligence/config/HttpSessionConfig.java
@@ -1,13 +1,12 @@
 package io.colligence.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.data.mongo.JacksonMongoSessionConverter;
-import org.springframework.session.data.mongo.config.annotation.web.http.EnableMongoHttpSession;
+import org.springframework.context.annotation.Configuration;
 
-@EnableMongoHttpSession
+@Configuration
+//@EnableMongoHttpSession
 public class HttpSessionConfig {
-    @Bean
-    JacksonMongoSessionConverter mongoSessionConverter() {
-        return new JacksonMongoSessionConverter();
-    }
+//    @Bean
+//    JacksonMongoSessionConverter mongoSessionConverter() {
+//        return new JacksonMongoSessionConverter();
+//    }
 }
--- src/main/java/io/colligence/persistence/repository/MongoCacheRepository.java
+++
@@ -1,9 +0,0 @@
-package io.colligence.persistence.repository;
-
-import io.colligence.persistence.model.MongoCache;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface MongoCacheRepository extends MongoRepository<MongoCache, Long> {
-}
